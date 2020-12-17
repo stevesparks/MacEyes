dc56e370b979293488a0c62c42a24068d8427f44
Steve Sparks <sparks@bignerdranch.com>
2020-12-17 15:52:31 -0500

    Initial Commit

---
+++ MacEyes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacEyes
+//
+//  Created by Steve Sparks on 12/17/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
