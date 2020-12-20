302bc22715f935890a0fbddd2095605873b16faa
Steve Sparks <sparks@bignerdranch.com>
2020-12-20 14:28:18 -0500

    Works!

--- MacEyes/AppDelegate.swift
+++ MacEyes/AppDelegate.swift
@@ -20,7 +20,38 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
+class Application: NSApplication {
+    override init() {
+        print("default")
+        super.init()
+    }
+    required init?(coder: NSCoder) {
+        print("coder")
+        super.init(coder: coder)
+    }
+}
+
+class AppWindow: NSWindow {
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: false)
+        alphaValue = 1.0
+        backgroundColor = .clear
+        isOpaque = false
+//        level = NSWindow.Level(rawValue: CGShieldingWindowLevel())
+    }
+
+    override func flagsChanged(with event: NSEvent) {
+        super.flagsChanged(with: event)
+print("flags")
+        var style = styleMask
+        if event.modifierFlags.contains(.shift) {
+            style.insert(.titled)
+        } else {
+            style.remove(.titled)
+        }
+        styleMask = style
+
+    }
+}
--- MacEyes/ViewController.swift
+++ MacEyes/ViewController.swift
@@ -8,19 +8,110 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    var tm: Timer?
 
+    @IBOutlet weak var rightEyeball: MouseFollowingEyeball!
+    @IBOutlet weak var leftEyeball: MouseFollowingEyeball!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+            self.flagsChanged(with: $0)
+            return $0
+        }
+        NSEvent.addGlobalMonitorForEvents(matching: .mouseMoved) { _ in
+            self.moveEyesToMouse()
+        }
+        NSEvent.addLocalMonitorForEvents(matching: .mouseMoved) {
+            self.moveEyesToMouse()
+            return $0
         }
     }
 
+    func moveEyesToMouse() {
+        leftEyeball.setNeedsDisplay(leftEyeball.bounds)
+        rightEyeball.setNeedsDisplay(rightEyeball.bounds)
+    }
 
+    override func flagsChanged(with event: NSEvent) {
+        super.flagsChanged(with: event)
+        print("viewcontroller \(event.modifierFlags.contains(.control))")
+    }
 }
 
+class MouseFollowingEyeball: NSView {
+    var eyeStrokeColor: NSColor = .black
+    var eyeFillColor: NSColor = .white
+
+    override func draw(_ dirtyRect: NSRect) {
+        guard let ctx = NSGraphicsContext.current?.cgContext else {
+            return
+        }
+
+        ctx.clear(bounds)
+        drawOuterEye(into: ctx)
+        drawPupil(into: ctx)
+    }
+
+    func drawOuterEye(into context: CGContext) {
+        let frm = bounds
+        let shortside = min(frm.size.width, frm.size.height)
+        let linewidth = shortside / 20.0
+
+        let drawFrame = bounds.insetBy(dx: linewidth/2, dy: linewidth/2)
+
+        context.addPath(CGPath(ellipseIn: drawFrame, transform: nil))
+        context.setStrokeColor(eyeStrokeColor.cgColor)
+        context.setLineWidth(linewidth)
+        context.setFillColor(eyeFillColor.cgColor)
+        context.drawPath(using: .fillStroke)
+    }
+
+    func drawPupil(into context: CGContext) {
+        context.setFillColor(eyeStrokeColor.cgColor)
+
+        let angle = angleInRadiansToMouseCursor
+        let mySin = (sin(angle) * 0.35) + 0.5
+        let myCos = (cos(angle) * 0.35) + 0.5
+
+        let rect = CGRect(x: mySin - 0.1, y: myCos - 0.1, width: 0.2, height: 0.2)
+        var xform = CGAffineTransform(scaleX: frame.size.width, y: frame.size.height)
+
+        withUnsafeMutablePointer(to: &xform) { ptr in
+            let path = CGPath(ellipseIn: rect, transform: ptr)
+            context.addPath(path)
+            context.drawPath(using: .fill)
+        }
+    }
+}
+
+extension NSView {
+    var angleInRadiansToMouseCursor: CGFloat {
+        let me = convert(frame, to: window?.contentView)
+        if let screenBounds = window?.convertToScreen(me) {
+            let loc = convert(NSEvent.mouseLocation, to: nil)
+            let ctr = screenBounds.center
+            let dx = ctr - loc
+            let angle = atan2(dx.x, dx.y)
+            return angle
+        } else {
+            return 0.0
+        }
+    }
+}
+
+
+extension CGRect {
+    var center: CGPoint {
+        return CGPoint(x: origin.x + (size.width / 2.0), y: origin.y + (size.height / 2.0))
+    }
+}
+
+extension CGPoint {
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: rhs.x - lhs.x, y: rhs.y - lhs.y)
+    }
+    static func -=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs.x -= rhs.x
+        lhs.y -= rhs.y
+    }
+}
